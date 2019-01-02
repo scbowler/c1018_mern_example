e82ef5c23dc661c3a4fb65b40fcf1b30315fdfeb
Scott Bowler <scott.bowler@learningfuze.com>
2019-01-02 13:28:24 -0800

    Project setup in progress

--- index.js
+++ index.js
@@ -5,9 +5,40 @@ const PORT = process.env.PORT || 9000;
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
     res.send('<h1>The server is working!</h1>');
+});
+
+app.get('/api/test', (req, res) => {
+    const data = {
+        success: true,
+        message: 'Test API working!',
+        another: 'Property goes here'
+    };
+
+    res.send(data);
+});
+
+app.get('/api/user', (req, res) => {
+    const user = {
+        email: 'jim@mail.com',
+        name: 'Jim Bob',
+        username: 'Jimmy'
+    }
+
+    res.send(user);
+});
+
+app.post('/api/send-message',  (req, res) => {
+    console.log('Data from client:', req.body);
+
+    res.send({
+        success: true,
+        dataReceived: req.body
+    });
 });
 
 app.listen(PORT, () => {
